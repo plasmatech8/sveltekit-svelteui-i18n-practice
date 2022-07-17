ee030f586e84deacc121d983dac31ba40473291f
Mark Connelly <com0004@gmail.com>
2022-07-17 13:16:26 +1000

    Init layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { AppShell, Navbar, Header, Aside, Footer, ShellSection, Burger } from '@svelteuidev/core';
+	let opened = false;
+</script>
+
+<AppShell>
+	<Header slot="header" height={60} override={{ p: '$mdPX' }}>
+		<Burger {opened} on:click={() => (opened = !opened)} />
+		Head
+	</Header>
+	<Navbar slot="navbar" hidden={!opened} width={{ base: 300 }}>Nav</Navbar>
+	<slot>This is the main content</slot>
+</AppShell>

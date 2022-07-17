c58daafdc26bf53f102ee9362eb29b1332a561d3
Mark Connelly <com0004@gmail.com>
2022-07-17 15:08:43 +1000

    Add navlinks & pages + SSR svelteUI provider

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
 	import '../app.css';
-	import { AppShell, Navbar, Header, Aside, Footer, ShellSection, Burger } from '@svelteuidev/core';
+	import {
+		AppShell,
+		Navbar,
+		Header,
+		Aside,
+		Footer,
+		ShellSection,
+		Burger,
+		Button,
+		SvelteUIProvider
+	} from '@svelteuidev/core';
 	let opened = false;
+
+	const links = [
+		{ href: '/', name: 'Home' },
+		{ href: '/foo', name: 'Foo' },
+		{ href: '/bar', name: 'Bar' }
+	];
 </script>
 
-<AppShell>
-	<Header slot="header" height={60} override={{ p: '$mdPX' }}>
-		<Burger {opened} on:click={() => (opened = !opened)} />
-		Head
-	</Header>
-	<Navbar slot="navbar" hidden={!opened} width={{ base: 300 }}>Nav</Navbar>
-	<slot>This is the main content</slot>
-</AppShell>
+<SvelteUIProvider ssr>
+	<AppShell>
+		<Header slot="header" height={60} override={{ p: '$mdPX' }}>
+			<Burger {opened} on:click={() => (opened = !opened)} />
+			Head
+		</Header>
+		<Navbar slot="navbar" hidden={!opened} width={{ base: 300 }}>
+			{#each links as link}
+				<Button class="point" variant="subtle" href={link.href}>{link.name}</Button>
+			{/each}
+		</Navbar>
+		<slot>This is the main content</slot>
+	</AppShell>
+</SvelteUIProvider>
